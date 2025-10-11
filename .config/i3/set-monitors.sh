b4511eba8587a4d38030c381acdbd03ea3683ce3
syphant <88693878+syphant@users.noreply.github.com>
2025-10-11 02:59:44 -0400

    Update monitor setup in set-monitors.sh

--- .config/i3/set-monitors.sh
+++ .config/i3/set-monitors.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sleep 1
-xrandr --output DP-4 --pos 0x0 --mode 1920x1080 --rate 60 \
-       --output DP-2 --pos 1920x0 --mode 2560x1440 --rate 144 \
-       --output DP-0 --pos 4480x0 --mode 1920x1080 --rate 60
+xrandr --output DP-2 --pos 0x0 --mode 1920x1080 --rate 60 \
+       --output DP-2 --rotate right \
+       --output DP-4 --pos 1080x0 --mode 2560x1440 --rate 165 \
 sleep 1
 feh --bg-fill --randomize ~/backgrounds/*
 picom
