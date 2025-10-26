ed4952470b71e717602bb4a831c7e77e47910dfa
syphant <syphant@users.noreply.github.com>
2025-10-26 17:54:07 -0400

    test fix for qt

--- install.sh
+++ install.sh
@@ -653,11 +653,11 @@ color_scheme_path=/usr/share/qt5ct/colors/darker.conf
 custom_palette=false
 icon_theme=Papirus-Dark
 standard_dialogs=default
-style=kvantum
+style=Fusion
 
 [Fonts]
-fixed=@Variant(\0\0\0@\0\0\0\x12\0M\0o\0n\0o\0s\0p\0\x61\0\x63\0\x65@$\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x10)
-general=@Variant(\0\0\0@\0\0\0\x12\0S\0\x61\0n\0s\0 \0S\0\x65\0r\0i\0\x66@$\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x10)
+fixed="Monospace,10,-1,5,50,0,0,0,0,0"
+general="Sans Serif,10,-1,5,50,0,0,0,0,0"
 
 [Interface]
 activate_item_on_single_click=1
@@ -685,11 +685,11 @@ color_scheme_path=/usr/share/qt6ct/colors/darker.conf
 custom_palette=false
 icon_theme=Papirus-Dark
 standard_dialogs=default
-style=kvantum
+style=Fusion
 
 [Fonts]
-fixed=@Variant(\0\0\0@\0\0\0\x12\0M\0o\0n\0o\0s\0p\0\x61\0\x63\0\x65@$\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x10)
-general=@Variant(\0\0\0@\0\0\0\x12\0S\0\x61\0n\0s\0 \0S\0\x65\0r\0i\0\x66@$\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x10)
+fixed="Monospace,10,-1,5,50,0,0,0,0,0"
+general="Sans Serif,10,-1,5,50,0,0,0,0,0"
 
 [Interface]
 activate_item_on_single_click=1
