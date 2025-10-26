70c0d3d31d5ae46a902162c1736decafaa8941be
syphant <88693878+syphant@users.noreply.github.com>
2025-10-26 11:37:45 -0400

    Add i3lock script for screen locking

---
+++ .config/i3/scripts/i3lock.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+CLEAR='#00000000'
+WHITE='#ffffffff'
+RED='#ff5555ff'
+GRAY='#333333ff'
+BLACK='#000000ff'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$BLACK   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$RED     \
+\
+--inside-color=$CLEAR        \
+--ring-color=$WHITE        \
+--line-color=$CLEAR          \
+--separator-color=$WHITE   \
+\
+--verif-color=$WHITE          \
+--wrong-color=$WHITE          \
+--time-color=$WHITE           \
+--date-color=$WHITE           \
+--layout-color=$WHITE         \
+--keyhl-color=$BLACK    \
+--bshl-color=$RED          \
+\
+--color=000000ff             \
+--indicator                  \
