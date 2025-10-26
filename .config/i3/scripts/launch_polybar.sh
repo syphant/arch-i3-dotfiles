5f58e380e9f9a6e03b319aaf88545ccc38da31c6
syphant <88693878+syphant@users.noreply.github.com>
2025-10-26 16:05:37 -0400

    Update launch_polybar.sh to improve polybar launch

--- .config/i3/scripts/launch_polybar.sh
+++ .config/i3/scripts/launch_polybar.sh
@@ -1,4 +1,13 @@
-if type "xrandr"; then
+#!/bin/bash
+
+# Terminate already running polybar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+
+# Launch polybar on all monitors
+if type "xrandr" &>/dev/null; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload toph &
   done
